d2230202a2d70d3ef4732e2ff1293dff9aa439fb
Red <leviethung1792k4@gmail.com>
2026-03-10 15:34:38 +0700

    add argon2

--- src/auth/hash/hash.module.ts
+++ src/auth/hash/hash.module.ts
@@ -3,12 +3,13 @@
 import { Module } from "@nestjs/common/decorators/modules/module.decorator";
 import { HASH_SERVICE } from "../constants/auth.constants";
 import { BcryptService } from "./services/bcrypt.service";
+import { Argon2Service } from "./services/argon2.service";
 
 @Module({
   providers: [
     {
       provide: HASH_SERVICE,
-      useClass: BcryptService,
+      useClass: Argon2Service,  // ← đổi thành Argon2Service
     },
   ],
   exports: [HASH_SERVICE],  // ← export để module khác dùng
